befdc7da55fbd701927e97fce55eb949a6c0f21e
CJ50353 <691068928@qq.comm>
2016-05-07 13:59:54 +0800

    11

--- 003-控制器/index.js
+++ 003-控制器/index.js
@@ -1,10 +1,18 @@
 /**
  * Created by taozhizhi on 2016/1/5.
  */
+angular.module('app',[])
+    .controller('firstController', function ($scope) {
+        //$scope是个作用域
+        //申明一个Model
+        $scope.name='z3';
+        $scope.age=20;
+        $scope.fn= function () {
+            $scope.age++;
+        }
+    })
+/*
 var firstController=function($scope){
-    //$scope是个作用域
-    //申明一个Model
-    $scope.name='z3';
-    $scope.age=20;
 
 }
+*/
--- 005-多个控制器/index.js
+++ 005-多个控制器/index.js
@@ -7,9 +7,26 @@ var firstController=function($scope){
     $scope.name='z3';
     console.log($scope);
 
+    $scope.$watch('name',function(newValue,oldValue){
+        if(newValue===oldValue){return;}
+        console.log('执行1');
+    })
 }
-
 var secondController=function($scope){
+
     $scope.name='x4';
+    setTimeout(function () {
+        $scope.$apply(function () {
+            $scope.name='x5';
+        })
+    },2000)
+    
+    $scope.$watch('name',function(newValue,oldValue){
+        if(newValue===oldValue){return;}
+        console.log('执行2');
+    })
     console.log($scope);
 }
+angular.module('app',[])
+    .controller('firstController',firstController)
+    .controller('secondController',secondController);
